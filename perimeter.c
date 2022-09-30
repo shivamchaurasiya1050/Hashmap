deca4f56e5bb772ab7356d6c39b7fd1c04c9faa2
shivamchaurasiya1050 <95376302+shivamchaurasiya1050@users.noreply.github.com>
2022-09-30 15:31:46 +0530

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,20 @@
+//Wap to find the perimeter of rectangle
+
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+{
+	float width,height;
+	
+	printf("ENTER THE WIDTH OF RECTENGLE \n");
+	
+	scanf("%f",&width);
+	
+	printf("ENTER THE HEIGHT OF RECTENGLE \n");
+	
+	scanf("%f",&height);
+	
+	printf("The parameters of rectangle is : %f\n",2*(height*width));
+}
