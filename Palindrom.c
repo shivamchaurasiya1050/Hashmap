7152e3dcf7b833b781f366aea6aee85bb63905f2
shivamchaurasiya1050 <95376302+shivamchaurasiya1050@users.noreply.github.com>
2022-09-30 15:31:02 +0530

    Add files via upload

---
+++ Palindrom.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int a,b=0,m,n;
+	printf("Enter the number:");
+	scanf("%d",&a);
+	n=a;
+	while(a>0){
+		m = a%10;
+		b = b*10+num;
+		a=(a/10);
+		
+		
+	}if(n==b){
+	
+	printf("Number is palindrome:");
+	}else{
+		printf("Number is not palindrome:");
+	}
+	return 0;
+	
+	
+		
+
+}
