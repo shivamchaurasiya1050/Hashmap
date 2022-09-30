74b0f0cd09ba7a24353e10a57f9213475a0c5dda
shivamchaurasiya1050 <95376302+shivamchaurasiya1050@users.noreply.github.com>
2022-09-30 15:24:56 +0530

    Add files via upload

---
+++ 1 se Armstrong.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a,sum=0,num,n;
+	
+	
+	for(a>=1;a>=10000;a++){
+		num = a%10;
+		sum = sum+(num*num*num);
+		//a=(a/10);
+		
+	}
+	printf("%d kjhgjghjg \n",&sum);
+		
+	
+	
+
+}
